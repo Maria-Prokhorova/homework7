a3ae0186eb9ba5ce4d64dfc11bf8660f0587be1a
Maria_Prokhorova <luchiksolnza@gmail.com>
2024-09-05 18:00:22 +0700

    Commit 1 - исправлены замечания

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,10 @@ public class Main {
         int salary = 15_000;
         int total = 0;
         while (total <= 2_459_000) {
-            total = total + salary;
-            month++;
+            total += total / 100;
+            total += salary;
             System.out.println("Месяц " + month + ", сумма накоплений равна " + total + " рублей.");
+            month++;
         }
         //System.out.println("Месяц " + month + ", сумма накоплений равна " + total + " рублей.");
         System.out.println();
@@ -32,16 +33,18 @@ public class Main {
         for (int year = 1; year <= 10; year++) {
             birthRate = 17 * y / 1000;
             deathRate = 8 * y / 1000;
-            y = y + birthRate - deathRate;
+            y += birthRate - deathRate;
             System.out.println("Год " + year + ", численность населения составляет " + y);
         }
         System.out.println();
 
         System.out.println("Задача 4");
         int sum = 15_000;
+        int percent = 7;
         month = 1;
-        while (sum < 12_000_000) {
-            sum = sum + 7 * sum / 100;
+        int totalSavings = 12_000_000;
+        while (sum < totalSavings) {
+            sum += percent * sum / 100;
             System.out.println(month + " месяц, сумма накоплений составла " + sum);
             month++;
         }
@@ -50,8 +53,8 @@ public class Main {
         System.out.println("Задача 5");
         sum = 15_000;
         month = 1;
-        while (sum < 12_000_000) {
-            sum = sum + 7 * sum / 100;
+        while (sum < totalSavings) {
+            sum += percent * sum / 100;
             if (month % 6 == 0) {
                 System.out.println(month + " месяц, сумма накоплений составла " + sum);
             }
@@ -62,8 +65,9 @@ public class Main {
         System.out.println("Задача 6");
         sum = 15_000;
         month = 1;
-        while (month <= 12 * 9) {
-            sum = sum + 7 * sum / 100;
+        int monthInYear = 12;
+        while (month <= monthInYear * 9) {
+            sum += percent * sum / 100;
             if (month % 6 == 0) {
                 System.out.println("Через " + month + " месяцев, сумма накоплений = " + sum);
             }
@@ -72,7 +76,7 @@ public class Main {
         System.out.println();
 
         System.out.println("Задача 7");
-        int friday = 7;
+        int friday = 5;
         for (int day = 1; day <= 31; day++) {
             if (day == friday) {
                 System.out.println("Сегодня пятница," + day + "-е число. Необходимо подготовить отчет.");
@@ -85,8 +89,9 @@ public class Main {
         int currentYear = 2024;
         int beforeYear = currentYear - 200;
         int afterYear = currentYear + 100;
+        int yearOfTheComet = 79;
         for (; beforeYear <= afterYear; beforeYear++) {
-            if (beforeYear % 79 == 0) {
+            if (beforeYear % yearOfTheComet == 0) {
                 System.out.println(beforeYear);
             }
         }
